2e1932044ce72512c07e7eab6047cfc18731846e
MatKapp <mka@bcsg.pl>
2019-12-07 14:21:27 +0100

    Removed custom patient POST method

--- PatientDiagnosis/patients/views.py
+++ PatientDiagnosis/patients/views.py
@@ -8,17 +8,11 @@ from rest_framework.decorators import action
 def index(request):
     return HttpResponse("Hello world-patients")
 
-class PatientView(viewsets.GenericViewSet, 
+class PatientView(viewsets.GenericViewSet,
+                    mixins.CreateModelMixin,
                     mixins.ListModelMixin,
                     mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin):
                     
     serializer_class = serializers.PatientSerializer
     queryset = models.Patient.objects.all()
-
-    @action(detail=True, methods=['post'])
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return response.Response(data=serializer.data, status=status.HTTP_201_CREATED)
