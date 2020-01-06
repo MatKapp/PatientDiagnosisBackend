2f939861e36d516ff90c60ca8752d860b338baed
MatKapp <mateusz.kapiszewski@gmail.com>
2020-01-06 18:12:58 +0100

    examination models extended

--- PatientDiagnosis/examinations/models.py
+++ PatientDiagnosis/examinations/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 
 class Examination(models.Model):
-    pulse = models.PositiveIntegerField(default=0)
+    atrial_fibrillation = models.BooleanField(default=False)
+    initial_dbp = models.PositiveIntegerField(default=0)
+    speech_dif = models.BooleanField(default=False)
+    gait_disturb = models.BooleanField(default=False)
+    body_weakness = models.BooleanField(default=False)
+    high_glucose = models.BooleanField(default=False)
+    first_tia = models.BooleanField(default=False)
+    vertigo = models.BooleanField(default=False)
+    infraction = models.PositiveIntegerField(default=0)
--- PatientDiagnosis/examinations/serializers.py
+++ PatientDiagnosis/examinations/serializers.py
@@ -5,4 +5,16 @@ from examinations import models
 class ExaminationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Examination
-        fields =('id', 'pulse')
+        fields =\
+        (
+            'id',
+            'atrial_fibrillation',
+            'initial_dbp',
+            'speech_dif',
+            'gait_disturb',
+            'body_weakness',
+            'high_glucose',
+            'first_tia',
+            'vertigo',
+            'infraction'
+        )
